9e851aacaade01ef50d4551b2068b64aa3403250
obara501 <geofreyongeta501@gmail.com>
2022-12-03 20:22:31 +0300

    First Commit

---
+++ admin/includes/header.php
@@ -0,0 +1,18 @@
+<div class="brand clearfix">
+	<div class="logo">
+		<a href="dashboard.php">Ann Electric | Admin Panel</a>
+	</div>
+
+	<span class="menu-btn"><i class="fa fa-bars"></i></span>
+
+	<ul class="ts-profile-nav">
+
+		<li class="ts-account">
+			<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+			<ul>
+				<li><a href="change-password.php">Change Password</a></li>
+				<li><a href="logout.php">Logout</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
---
+++ admin/includes/leftbar.php
@@ -0,0 +1,23 @@
+	<nav class="ts-sidebar">
+		<ul class="ts-sidebar-menu">
+			
+			<li><a href="dashboard.php"><i class="fa fa-dashboard"></i>Dashboard</a></li>
+
+			<li><a href="#"><i class="fa fa-files-o"></i>Categories</a>
+				<ul>
+					<li><a href="create-category.php">Create Category</a></li>
+					<li><a href="manage-category.php">Manage Categories</a></li>
+				</ul>
+			</li>
+
+			<li><a href="#"><i class="fa fa-sitemap"></i>Products</a>
+				<ul>
+					<li><a href="post-alaptop.php">Add a Product</a></li>
+					<li><a href="manage-laptops.php">Manage Products</a></li>
+				</ul>
+			</li>
+			<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i>Manage ContactUs Query</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i>Update Contact Info</a></li>
+
+		</ul>
+	</nav>
